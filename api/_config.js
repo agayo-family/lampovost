6b02370706cda95038754fd1e1f744408776bd7f
agayo-family <ergin.sergey2002@gmail.com>
2026-09-15 00:24:05 +0300

    м12

--- api/_config.js
+++
@@ -1,4 +0,0 @@
-const PUBLIC_KEY = 'https://disk.yandex.ru/d/H10Ne-YjYh7yzQ';
-const API_BASE = 'https://cloud-api.yandex.net/v1/disk/public/resources';
-
-module.exports = { PUBLIC_KEY, API_BASE };
